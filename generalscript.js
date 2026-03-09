b376be4d752a59d63e8d1a8890042e4ec111e8c7
Leeds-student-radio <info@thisislsr.com>
2026-03-09 17:48:52 +0100

    Update generalscript.js

--- generalscript.js
+++ generalscript.js
@@ -835,7 +835,7 @@ function initChatSystem() {
         gifResults.innerHTML = '<p style="text-align:center; grid-column: 1 / -1;">Loading...</p>';
         
         // Put your hand-picked GIF IDs here, separated by commas (no spaces)
-        const myFavoriteGifs = "l3vRlT2k2L35Cnn5C,ZqlvCTNHpqrio,FdRzET4jjKt4HVzri7,wW95fEq09hOI8,GWKQzZX7bNqRMO6bMw,mGK1g88HZRa2FlKGbz,fX5cZemSfX1cMZYuUJ,gKHGnB1ml0moQdjhEJ,kCpg2FYkENfnuvXSsS,QxcSqRe0nllClKLMDn,g88xUM1rTwjfLhoRYP,13hxeOYjoTWtK8,Zuv1Q8Ao8yxQ49OiXU"; 
+        const myFavoriteGifs = "l3vRlT2k2L35Cnn5C,ZqlvCTNHpqrio,FdRzET4jjKt4HVzri7,wW95fEq09hOI8,GWKQzZX7bNqRMO6bMw,mGK1g88HZRa2FlKGbz,fX5cZemSfX1cMZYuUJ,gKHGnB1ml0moQdjhEJ,kCpg2FYkENfnuvXSsS,QxcSqRe0nllClKLMDn,g88xUM1rTwjfLhoRYP,13hxeOYjoTWtK8,Zuv1Q8Ao8yxQ49OiXU,3o72FcJmLzIdYJdmDe"; 
         
         // If the search is empty, fetch your specific GIFs. Otherwise, search Giphy.
         const endpoint = searchTerm.trim() === '' 
