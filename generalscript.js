ee2aa84583b866291647ca384bf676b40671a6c2
Leeds-student-radio <info@thisislsr.com>
2026-03-13 00:23:00 +0000

    Update generalscript.js

--- generalscript.js
+++ generalscript.js
@@ -841,7 +841,7 @@ if (gifUrl) {
         gifResults.innerHTML = '<p style="text-align:center; grid-column: 1 / -1;">Loading...</p>';
         
         // Put your hand-picked GIF IDs here, separated by commas (no spaces)
-        const myFavoriteGifs = "l3vRlT2k2L35Cnn5C,mBdbauuNxUpnqr1B1u,FdRzET4jjKt4HVzri7,wW95fEq09hOI8,GWKQzZX7bNqRMO6bMw,mGK1g88HZRa2FlKGbz,SDeVLvFCqFsSA,RX7N03MEUafW8,gKHGnB1ml0moQdjhEJ,kCpg2FYkENfnuvXSsS,QxcSqRe0nllClKLMDn,g88xUM1rTwjfLhoRYP,13hxeOYjoTWtK8,3o72FcJmLzIdYJdmDe,ujTVMASREzuRbH6zy5"; 
+        const myFavoriteGifs = "l3vRlT2k2L35Cnn5C,mBdbauuNxUpnqr1B1u,FdRzET4jjKt4HVzri7,wW95fEq09hOI8,GWKQzZX7bNqRMO6bMw,mGK1g88HZRa2FlKGbz,SDeVLvFCqFsSA,RX7N03MEUafW8,gKHGnB1ml0moQdjhEJ,qzlUJOV5ON8XkHbO53,kCpg2FYkENfnuvXSsS,QxcSqRe0nllClKLMDn,g88xUM1rTwjfLhoRYP,13hxeOYjoTWtK8,3o72FcJmLzIdYJdmDe,ujTVMASREzuRbH6zy5"; 
         
         // If the search is empty, fetch your specific GIFs. Otherwise, search Giphy.
         const endpoint = searchTerm.trim() === '' 
