a98a8174832339e451115a2bc522ab6f713da6c4
Leeds-student-radio <info@thisislsr.com>
2026-03-16 15:01:52 +0000

    Update delete.js

--- delete.js
+++ delete.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, query, where, getDocs, writeBatch, Timestamp } from "firebase/firestore";
 
 // Your exact config
+import 'dotenv/config'; // Loads the .env file
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDSGLLwH1BVYQVY1FLkAUe3XUmIYu2Nfhc",
-  authDomain: "lsrchat-6ffb1.firebaseapp.com",
-  projectId: "lsrchat-6ffb1",
-  storageBucket: "lsrchat-6ffb1.firebasestorage.app",
-  messagingSenderId: "333921149565",
-  appId: "1:333921149565:web:c5dcced8299b1527994714"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
