89f66c92240c7d7cb483bced9a68bcb23ff43a39
Leeds-student-radio <info@thisislsr.com>
2026-03-16 15:56:50 +0000

    Update generalscript.js

--- generalscript.js
+++ generalscript.js
@@ -752,7 +752,7 @@ function getLondonTimeDetails() {
 // --- CONFIGS ---
 const chatConfig = {
     apiKey: "#{FIREBASE_API_KEY}#",
-    authDomain: "#{FIREBASE_AUTH_DOMAIN}#",
+    authDomain: "#{FIREBASE_AUTH_DOMAIN}#", 
     projectId: "#{FIREBASE_PROJECT_ID}#",
     storageBucket: "#{FIREBASE_STORAGE_BUCKET}#",
     messagingSenderId: "#{FIREBASE_MESSAGING_SENDER_ID}#",
