0b1cbc61e157409c7fb0b9c65d84291dc7d75378
Leeds-student-radio <info@thisislsr.com>
2026-03-22 21:08:30 +0000

    Deploying to gh-pages from @ Leeds-student-radio/Lsr@c97408a1b76c85df18b094da216bfb932ce0fde6 🚀

--- generalscript.js
+++ generalscript.js
@@ -1085,17 +1085,7 @@ function initChatSystem() {
         }
     }
     
-const input = document.querySelector('input');
 
-input.addEventListener('focus', (e) => {
-  // Use a slight timeout to let the keyboard begin to slide up
-  setTimeout(() => {
-    e.target.scrollIntoView({
-      block: 'end',      // Aligns the bottom of the element with the bottom of the view
-      behavior: 'smooth' // Optional: makes it look less jarring
-    });
-  }, 300);
-});
     // --- 3. GIF PICKER LOGIC ---
     if (gifToggleBtn && gifPicker && closeGifBtn) {
         gifToggleBtn.addEventListener('click', () => {
