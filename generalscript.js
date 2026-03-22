778a0256027e854ee4b7926c1ccb7d198dd10c8e
Leeds-student-radio <info@thisislsr.com>
2026-03-22 14:53:06 +0000

    Deploying to gh-pages from @ Leeds-student-radio/Lsr@cd91e0a5acef84265aaa476586ce9edc1c5d6234 🚀

--- generalscript.js
+++ generalscript.js
@@ -877,7 +877,7 @@ function initChatSystem() {
     } else {
    
    // Using the Adventurer style, randomized IDs, and flipping the avatar horizontally
-   avatarImg.src = `https://api.dicebear.com/9.x/fun-emoji/svg?seed=${encodeURIComponent(name)}&randomizeIds=true&backgroundColor=71cf62,fcbc34,FF595E&scale=90&backgroundType=gradientLinear&mouth=cute,lilSmile,shout,smileLol,tongueOut&eyes=closed,cute,glasses,shades,stars,wink2,crying`;   }
+   avatarImg.src = `https://api.dicebear.com/9.x/fun-emoji/svg?seed=${encodeURIComponent(name)}&randomizeIds=true&backgroundColor=71cf62,fcbc34,FF595E&scale=90&backgroundType=gradientLinear&mouth=cute,lilSmile,shout,smileLol,tongueOut&eyes=closed,cute,glasses,stars,wink2,crying`;   }
 
     // Fallback handler if DiceBear fails
     avatarImg.onerror = function() {
